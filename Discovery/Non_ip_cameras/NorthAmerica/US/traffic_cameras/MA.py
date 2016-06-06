3b672636837898777e4f3b0d02450974d49cbae2
cwengyan <cwengyan@purdue.edu>
2016-06-06 14:26:29 -0400

    Update header

--- Discovery/Non_ip_cameras/NorthAmerica/US/traffic_cameras/MA.py
+++ Discovery/Non_ip_cameras/NorthAmerica/US/traffic_cameras/MA.py
@@ -1,10 +1,23 @@
-
 #!/usr/bin/python
-
+""" 
+--------------------------------------------------------------------------------
+Descriptive Name     : MA.py
+Author               : unknown							      
+Contact Info         : ssui@purdue.edu (Shengli Sui)
+Description          : Parse cameras on the 511 Massachusetts traffic camera website
+Command to run script: python MA.py
+Output               : output MA.xml 
+Other files required by : N/A
+this script and where 
+located
+----For Parsing Scripts---------------------------------------------------------
+URL Parse	     : http://www1.eot.state.ma.us/xmltrafficfeed/camsXML.aspx
+In database (Y/N)    : Y
+--------------------------------------------------------------------------------
+"""
 import urllib2
 import httplib
 
-#This program is to receive XML feed from Ohio state
 def getMA():	
 	URL= 'http://www1.eot.state.ma.us/xmltrafficfeed/camsXML.aspx'
 	xml = urllib2.urlopen(URL)
