be8e01c739bcc9294ecf2743fc377a81a1d6b1ea
cwengyan <cwengyan@purdue.edu>
2016-06-06 14:31:31 -0400

    Update header

--- Discovery/Non_ip_cameras/NorthAmerica/US/traffic_cameras/MD.py
+++ Discovery/Non_ip_cameras/NorthAmerica/US/traffic_cameras/MD.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
-
+""" 
+--------------------------------------------------------------------------------
+Descriptive Name     : MD.py
+Author               : unknown							      
+Contact Info         : ssui@purdue.edu (Shengli Sui)
+Description          : Parse cameras on the Maryland Dept of Transportation traffic camera website
+Command to run script: python MD.py
+Output               : output MD.xml 
+Other files required by : N/A
+this script and where 
+located
+----For Parsing Scripts---------------------------------------------------------
+URL Parse	     : http://www.chart.state.md.us/rss/ProduceRss.aspx?Type=VIDEOXML&filter=all
+In database (Y/N)    : Y
+--------------------------------------------------------------------------------
+"""
 import urllib2
 import httplib
 
