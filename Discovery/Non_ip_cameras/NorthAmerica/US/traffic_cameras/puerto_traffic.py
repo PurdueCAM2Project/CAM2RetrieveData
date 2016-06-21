ac49fae768ebd7572982bb4b1c198ea141956254
Sanghyun Joo <joos@purdue.edu>
2016-06-21 10:21:30 -0400

    Fixed the main for better readability

--- Discovery/Non_ip_cameras/NorthAmerica/US/traffic_cameras/puerto_traffic.py
+++ Discovery/Non_ip_cameras/NorthAmerica/US/traffic_cameras/puerto_traffic.py
@@ -26,6 +26,9 @@ Date added to Database : June 13 1026
 
 import selenium.webdriver.support.ui as ui
 import time
+import urllib
+import re
+import traceback
 from Geocoding import Geocoding
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -33,43 +36,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
 
 class Puerto:
     def __init__(self):
         # store the url of homepage, traffic page, the country code, and the state code
-        self.home_url = "http://its.dtop.gov.pr/es/"
+        self.home_url = "http://its.dtop.gov.pr"
+        self.parent_url = "http://its.dtop.gov.pr/es/"
         self.traffic_url = "http://its.dtop.gov.pr/es/TrafficCameras.aspx?"
         self.country = "USA"
         self.state = "PR"
 
-        # open web browser for selenium
-        self.driver = webdriver.Firefox()
-
         # open the file to store the list and write the format of the list at the first line
         self.f = open('list_puerto_traffic', 'w')
         self.f.write("city#country#state#snapshot_url#latitude#longitude" + "\n")
+        
+    def get_soup(self, url):
+        """ Create beautifulSoup object with the given url and return it
 
-    def get_data(self, link):
-        """ Get the url to the img_src of the given link
+            Args:
+                url: the URL address of the webpage to be parsed
+
+            Return:
+                soup: beautifulSoup object to parse the given URL
+        """
+        soup_url = urllib.urlopen(url.encode("UTF-8")).read()
+        soup = BeautifulSoup(soup_url, "html.parser")
+
+        return soup
+
+    def get_data(self, a_tag):
+        """ Get the img_src of the given camera 
                 the detailed location information
                 the city name about one camera
 
-            The arguement link is a Selenium element that contains the html data about one camera.
+            The arguement link is a BeautifulSoup element that contains the html data about one camera.
             This function extracts the needed data in the <a href=""> tag.
 
             Args:
-                link: Selenium element that contains the html data about one camera
+                a_tag: Selenium element that contains the html data about one camera
 
             Return:
-                cam_src: the extracted link url from <a href=""> tag
+                img_src: the img_src of the given camera
                 desc: the detailed location information
                 city: the city name about one camera
         """
-        cam_src = link.get_attribute('href')
-        city = link.text.split("-")[0]
-        desc = self.get_token(link.text.encode("UTF-8"), "(", ")").strip()
+        link_to_cam = a_tag.get('href')
 
-        return cam_src, city, desc
+        img_src = self.get_img_src(self.parent_url + link_to_cam)
+        city = a_tag.text.split("-")[0]
+        desc = self.get_token(a_tag.text.encode("UTF-8"), "(", ")").strip()
+
+        return img_src, city, desc
     
     def get_token(self, camera, front, end):
         """ Extract the substring between <front> and <end> string
@@ -98,8 +116,8 @@ class Puerto:
         """ Get the img_src of one camera
             
             The cam_src contains the link to the webpage that has img_src.
-            Moves to that webpage and get the img_src on the page.
-            Then, moves back and returns the img_src
+            Create parser for that webpage and get the img_src on the page.
+            Then, return the img_src
 
             Args:
                 cam_src: the link to the webpage that has img_src
@@ -107,48 +125,30 @@ class Puerto:
             Return:
                 img_src: image url of one camera
         """
-        self.driver.get(cam_src)
+        soup_cam = self.get_soup(cam_src)
 
-        img_tag = self.driver.find_element_by_id("Image")
-        img_src = img_tag.get_attribute("src").split("?")[0]
-
-        self.driver.back()
+        img_tag = soup_cam.find("img")
+        img_src = img_tag.get("src").split("?")[0]
+        img_src = self.home_url + img_src
 
         return img_src
     
     def main(self):
-        # get parser for the homepage
-        self.driver.get(self.traffic_url)
-        variable = Geocoding('Nominatim', None)
+        # create parser for the traffic webpage
+        soup_traffic = self.get_soup(self.traffic_url)
+        geo = Geocoding('Nominatim', None)
 
-        # list to store cam_src, city name, and descriptioon about each camera
-        cam_srcs = []
-        cities = []
-        descrips = []
+        # loop through the a_tag about each camera
+        soup_traffic_links = soup_traffic.find("div", {"id" : "bodyContent_bodyContent_cctv"})
+        for a_tag in soup_traffic_links.findAll("a", {"target" : "_blank"}):
+            # get the im_src, city name, and description about the camera
+            img_src, city, descrip = self.get_data(a_tag)
 
-        # Selenium element that contains a list of links of all cameras
-        link_container = self.driver.find_element_by_id("bodyContent_bodyContent_cctv")
-
-        # loop through the links of cameras to get the data
-        for link in link_container.find_elements_by_tag_name("a"):
-            cam_src, city, desc = self.get_data(link)
-
-            # store the parsed data into the list
-            cam_srcs.append(cam_src)
-            cities.append(city)
-            descrips.append(desc)
-
-        # loop through the parsed data of cameras to write to the file
-        for i in range(len(cam_srcs)):
-            img_src = self.get_img_src(cam_srcs[i])
-            city = cities[i]
-            descrip = descrips[i]
-            
             print(city, descrip, img_src)
             
             try:
-                variable.locateCoords(descrip, city, self.state, self.country)
-                self.f.write(variable.city.replace(" ", "").title() + "#" + variable.country + "#" + variable.state + "#" + img_src + "#" + variable.latitude + "#" + variable.longitude + "\n")
+                geo.locateCoords(descrip, city, self.state, self.country)
+                self.f.write(geo.city.replace(" ", "").title() + "#" + geo.country + "#" + geo.state + "#" + img_src + "#" + geo.latitude + "#" + geo.longitude + "\n")
             except:
                 print("can't find")
 
