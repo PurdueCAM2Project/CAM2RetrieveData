c59f26094052daf2d576656032c6f073fcea4c5a
Thomas Norling <tnorling@purdue.edu>
2016-06-23 09:42:45 -0400

    Update Output text file name

--- Discovery/Non_ip_cameras/Asia/Hongkong_CN.py
+++ Discovery/Non_ip_cameras/Asia/Hongkong_CN.py
@@ -60,7 +60,7 @@ def getHK():
 	response = opener.open(url)
 	page = response.read()
 	soup = BeautifulSoup(page) # Create soup
-	f = open('list_Hongkong_CN','w') # Create file for output
+	f = open('list_Hongkong_CN.txt','w') # Create file for output
 	f.write('snapshot_url#description#latitude#longitude#country#city')
 	f.write('\n')
 	for tag in soup.find_all('image'):
