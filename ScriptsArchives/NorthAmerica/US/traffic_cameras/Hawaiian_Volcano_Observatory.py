5240a37c05c597bf2bd2dc39c48b3182770d78cb
cwengyan <cwengyan@purdue.edu>
2016-06-24 16:54:00 -0400

    Update header

--- ScriptsArchives/NorthAmerica/US/traffic_cameras/Hawaiian_Volcano_Observatory.py
+++ ScriptsArchives/NorthAmerica/US/traffic_cameras/Hawaiian_Volcano_Observatory.py
@@ -6,7 +6,7 @@ Contact Info         : ssui@purdue.edu (Shengli Sui)
 Description          : Parse cameras on the Hawaiian_Volvano_Observatory camera website
 Command to run script: python Hawaiian_Volvano_Observatory.py
 Output               : output urls, country, city and latitude, longitude to a 
-                       textfile <list_HVO>
+                       textfile <list_Hawaiian_Volcano_Observatory.txt>
 Other files required by : N/A
 this script and where 
 located
