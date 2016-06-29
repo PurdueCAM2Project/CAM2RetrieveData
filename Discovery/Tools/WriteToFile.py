f2defd0397b4228306ec3767ffa902a998373154
Thomas Norling <tnorling@purdue.edu>
2016-06-29 14:24:33 -0400

    Added a sleep to allow time to write to file

--- Discovery/Tools/WriteToFile.py
+++ Discovery/Tools/WriteToFile.py
@@ -23,14 +23,19 @@
 # DO NOT PUT USERNAMES/PASSWORDS IN CODE
 ###############################################################################
 
+import time
+
 class WriteToFile:
     def __init__(self,states,filename):
         self.f = open(filename, 'w')
         if states == True:  
             self.f.write("country#state#city#snapshot_url#latitude#longitude\n")
+            time.sleep(0.5)
         else:
             self.f.write("country#city#snapshot_url#latitude#longitude\n")
+            time.sleep(0.5)
     
     def writeInfo(self, country, state, city, link, latitude, longitude):
         info = str(country) + '#' + state + '#' + str(city) + '#' + link + '#' + latitude + '#' + longitude
         self.f.write(info.encode('utf-8').replace(" ","").replace("##", "#").replace("\n",'')+'\n')
+        time.sleep(1)
