a20cf75bedb1a7f0ff9f7af8ff9ff8e9afd4511b
Sanghyun Joo <joos@purdue.edu>
2016-07-18 16:33:15 -0400

    Inherit Useful.py for simpler code

--- ScriptsArchives/NorthAmerica/Canada/traffic_cameras/NewBrunswick.py
+++ ScriptsArchives/NorthAmerica/Canada/traffic_cameras/NewBrunswick.py
@@ -10,7 +10,7 @@ Usage                : N/A
 Input file format    : N/A
 Output               : list_NewBrunswick.txt
 Note                 : 
-Other files required by : Geocoding.py from in NetworkCameras/Discovery/Tools
+Other files required by : Geocoding.py and Useful.py in NetworkCameras/Discovery/Tools
 this script and where     It requires Selenium and BeautifulSoup4 to be installed
 located
 
@@ -29,6 +29,7 @@ import urllib
 import re
 import traceback
 from Geocoding import Geocoding
+from Useful import Useful
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -37,7 +38,7 @@ from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
-class NewBrunswick:
+class NewBrunswick(Useful):
     def __init__(self):
         # store the url of homepage, traffic page, the country code, and the state code
         self.home_url = "http://www2.gnb.ca"
@@ -48,53 +49,7 @@ class NewBrunswick:
         # open the file to store the list and write the format of the list at the first line
         self.f = open('list_NewBrunswick.txt', 'w')
         self.f.write("city#country#snapshot_url#latitude#longitude" + "\n")
-
-    def get_soup(self, url):
-        """ Create beautifulSoup object with the given url and return it
-
-            Args:
-                url: the URL address of the webpage to be parsed
-
-            Return:
-                soup: beautifulSoup object to parse the given URL
-        """
-        soup_url = urllib.urlopen(url.encode("UTF-8")).read()
-        soup = BeautifulSoup(soup_url, "html.parser")
-
-        return soup
-
-    def get_token(self, string, front, end):
-        """ Extract the substring between <front> and <end> string
-            
-            The string contains string or html element
-            This function extract the substring between <front> and <end> string
-            If front string is empty, return string from the first character to the split of end string
-            If end string is empty, return string from the end character to the split of the front string
-
-            Args:
-                string: string or html element
-                front: string at the left of the wanted substring
-                end: string at the right of the wanted substring
-
-            Return:
-                token: the string between <front> and <end> string OR if DNE, return empty string
-        """
-        try:
-            s = str(string)
-            if front == "":
-                token = s.split(end)[0]
-            elif end == "":
-                token = s.split(front)[1]
-            else:
-                front_split = s.split(front)[1]
-                token = front_split.split(end)[0]
-        except:
-            print("get_token error")
-            traceback.print_exc()
-            token = ""
-
-        return token
-
+    
     def get_data(self, a_tag):
         """ Get the description, image url, and city name of the given camera
 
@@ -120,7 +75,7 @@ class NewBrunswick:
     def main(self):
         # get parser for the traffic page
         geo = Geocoding('Google', None)
-        soup_traffic = self.get_soup(self.traffic_url)
+        soup_traffic = Useful.get_parser_with_soup(self, self.traffic_url)
 
         # loop through each link
         for a_tag in soup_traffic.findAll("a", {"class" : "thumbnail"}):
