e1607074b4a52d65c8edfe06d8c2a8fa7d064b3f
Sanghyun Joo <joos@purdue.edu>
2016-07-21 10:14:10 -0400

    Add files via upload

--- ScriptsArchives/Europe/traffic_cameras/NO_onthesnow.py
+++ ScriptsArchives/Europe/traffic_cameras/NO_onthesnow.py
@@ -1,23 +1,23 @@
 """
 --------------------------------------------------------------------------------
-Descriptive Name     : IE_dublincity.py
+Descriptive Name     : NO_onthesnow.py
 Author               : Sanghyun Joo
 Contact Info         : joos@purdue.edu OR toughshj@gmail.com
-Date Written         : 19 July 2016
+Date Written         : 20 July 2016
 Description          : parses the city name, snapshot_url, latitude, and longitude for each camera
-Command to run script: python IE_dublincity.py
+Command to run script: python NO_onthesnow.py
 Usage                : N/A
 Input file format    : N/A
-Output               : list_Ireland_dublincity.txt
+Output               : list_Norway_onthesnow.txt
 Note                 : 
 Other files required by : Geocoding.py, CameraData.py, and Useful.py in NetworkCameras/Discovery/Tools
 this script and where     It requires Selenium and BeautifulSoup4 to be installed.
 located
 
 ----For Parsing Scripts---------------------------------------------------------
-Website Parsed       : https://www.dublincity.ie/dublintraffic/
+Website Parsed       : http://www.onthesnow.com/norway/webcams.html
 In database (Y/N)    : Y
-Date added to Database : 19 July 2016
+Date added to Database : 20 July 2016
 --------------------------------------------------------------------------------
 """
 
@@ -39,47 +39,53 @@ from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
-class Ireland(Useful):
+class Norway(Useful):
     def __init__(self):
         # store the url of homepage, traffic page, the country code, and the state code
-        self.home_url       = "https://www.dublincity.ie"
-        self.traffic_url    = "https://www.dublincity.ie/dublintraffic/"
-        self.country        = "IE"
+        self.home_url       = "http://www.onthesnow.com"
+        self.traffic_url    = "http://www.onthesnow.com/norway/webcams.html"
+        self.country        = "NO"
         self.state          = ""
 
         # open the file to store the list and write the format of the list at the first line
-        self.list_file = open('list_Ireland_dublincity.txt', 'w')
+        self.list_file = open('list_Norway_onthesnow.txt', 'w')
         self.list_file.write("city#country#snapshot_url#latitude#longitude" + "\n")
 
         # gps module
         self.gps = Geocoding('Google', None)
     
     def get_camera_data(self, cam):
-        img_tag = cam.find("img")
-
-        img_src     = img_tag.get("src")
-        description = img_tag.get("alt")
-        city        = self.get_city(description)
+        img_src     = self.get_img_src(cam)
+        city        = self.get_city(cam)
+        description = city
         state       = self.state
         country     = self.country
+        
+        print(img_src, city)
 
         camera_data = CameraData(img_src, country, state, city, description)
 
         return camera_data
 
-    def get_city(self, description):
-        city = description
-        if "-" in city:
-            city = Useful.get_token_between(self, city, "-", "")
-        if "/" in city:
-            city = Useful.get_token_between(self, city, "/", "")
+    def get_img_src(self, cam):
+        img_tag = cam.find("img")
+        img_src = img_tag.get('data-original')
 
-        return city.strip()
+        return img_src
+
+    def get_city(self, cam):
+        text = cam.find("strong").text
+        city = Useful.get_token_between(self, text, "", "webcams").strip()
+
+        if "-" in city:
+            city = Useful.get_token_between(self, text, "-", "webcams").strip()
+
+        return city
 
     def main(self):
         # loop through each camera
         parser_for_traffic_webpage = Useful.get_parser_with_soup(self, self.traffic_url)
-        for cam in parser_for_traffic_webpage.findAll("span", {"class" : "detContent"}):
+        for cam in parser_for_traffic_webpage.findAll("p", {"class" : "col_3"}):
             try:
                 camera_data     = self.get_camera_data(cam)
                 input_format    = self.convert_parsed_data_into_input_format(camera_data)
@@ -89,5 +95,5 @@ class Ireland(Useful):
                 traceback.print_exc()
 
 if __name__ == '__main__':
-    Ireland = Ireland()
-    Ireland.main()
+    Norway = Norway()
+    Norway.main()
