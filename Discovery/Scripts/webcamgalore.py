b6bf9a835ceb76a64d5e65608d3e7b385ac86e26
Ali Çataltepe <Ali-Cataltepe@users.noreply.github.com>
2016-07-21 10:18:45 -0400

    Update webcamgalore.py

--- Discovery/Scripts/webcamgalore.py
+++ Discovery/Scripts/webcamgalore.py
@@ -54,7 +54,6 @@ def webcamgaloreNavigate():
 		masterList="https://www.webcamgalore.com/EN/complete-"+i+".html"
 		driver.get(masterList);
 		time.sleep(1)
-		linklist = []
 		for elem in driver.find_elements_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/a"):
 			print elem.get_attribute("href")
 			cameraPageLinks.append(elem.get_attribute("href"))
@@ -67,80 +66,107 @@ def webcamgaloreNavigate():
 	time.sleep(0.5)
 	for i in cameraPageLinks:
 		driver.get(i)
-		locationString = get_text_excluding_children(driver, driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/h1"))
-		if "Several" not in locationString:
-			country = locationString.split(":")[0].split(",")[-1].strip()
-			city = locationString.split(":")[0].split(",")[0].strip()
-			locat = locationString.split(":")[1].strip()
-			locatwords = locat.split()
-			resultwords  = [word for word in locatwords if word.lower() not in stopwords]
-			locat = ' '.join(resultwords)
-			state = ""
-			if len(locationString.split(":")[0].split(",")) > 2:
-				locat+=(" "+locationString.split(":")[0].split(",")[1])
-			try:
-				snapshot_link = urllib.quote(driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/table[2]/tbody/tr/td[@style='padding-left:10px']/center/a/img").get_attribute("src"), safe = ':?,=/&')
-			except:
-				snapshot_link = urllib.quote(driver.find_element_by_class_name("webcam-image").get_attribute("src"), safe = ':?,=/&')
-			try:
-				geocoder.locateCoords(locat, city, state, country)
-				geocoder.reverse(geocoder.latitude, geocoder.longitude)
-				info = geocoder.country + '#' + state + '#' + geocoder.city + '#' + snapshot_link + '#' + str(geocoder.latitude) + '#' + str(geocoder.longitude)
-				listfile.write(info.encode('utf-8').replace(" ","").replace("##", "#").replace("\n",'')+'\n')
-				time.sleep(1.0)
-			# API key cycling courtesy of Ryan
-			except Exception as error:
-				if str(error) == "Your request was denied.":
-					API_num = API_num+1
-					if API_num < len(geocodingKeys):
-						geocoder = Geocoding("Google", geocodingKeys[API_num])
+		try:
+			locationString = get_text_excluding_children(driver, driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/h1"))
+			if "Several" not in locationString:
+				country = locationString.split(":")[0].split(",")[-1].strip()
+				city = locationString.split(":")[0].split(",")[0].strip()
+				locat = locationString.split(":")[1].strip()
+				locatwords = locat.split()
+				resultwords  = [word for word in locatwords if word.lower() not in stopwords]
+				locat = ' '.join(resultwords)
+				state = ""
+				print locat
+				locat = locat.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				state = state.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				city = city.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				country = country.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				if len(locationString.split(":")[0].split(",")) > 2:
+					locat+=(" "+locationString.split(":")[0].split(",")[1])
+				try:
+					snapshot_link = urllib.quote(driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/table[2]/tbody/tr/td[@style='padding-left:10px']/center/a/img").get_attribute("src"), safe = ':?,=/&')
+				except:
+					try:
+						snapshot_link = urllib.quote(driver.find_element_by_class_name("webcam-image").get_attribute("src"), safe = ':?,=/&')
+					except:
+						pass
+				try:
+					geocoder.locateCoords(locat, city, state, country)
+					time.sleep(1.5)
+					geocoder.reverse(geocoder.latitude, geocoder.longitude)
+					time.sleep(1.5)
+					info = geocoder.country + '#' + state + '#' + geocoder.city + '#' + snapshot_link + '#' + str(geocoder.latitude) + '#' + str(geocoder.longitude)
+					print info
+					listfile.write(info.replace(" ","").replace("##", "#").replace("\n",'')+'\n')
+					time.sleep(1.0)
+				# API key cycling courtesy of Ryan
+				except Exception as error:
+					if str(error) == "Your request was denied." or str(error) == "The given key has gone over the requests limit in the 24 hour period or has submitted too many requests in too short a period of time.":
+						API_num = API_num+1
+						if API_num < len(geocodingKeys):
+							geocoder = Geocoding("Google", geocodingKeys[API_num])
+						else:
+							print "<<<ERROR:"+str(error)+">>>"
+							print "API list exhausted, stopped on:"
+							print geocoder.country+"#"+snapshot_link+"#"+str(geocoder.latitude)+"#"+str(geocoder.longitude)+"#"+"ERROR"+"#"+"ERROR"
+							return
 					else:
-						print "<<<ERROR:"+str(error)+">>>"
-						print "API list exhausted, stopped on:"
-						print country+"#"+url+"#"+str(latitude)+"#"+str(longitude)+"#"+"ERROR"+"#"+"ERROR"
-						return
-				else:
-					print "<<<Error:"+str(error)+">>>"+" For:\n"+country+"#"+url+"#"+str(latitude)+"#"+str(longitude)+"#"+"ERROR"+"#"+"ERROR"
-					pass
+						print "<<<Error:"+str(error)+">>>"+" For:\n"+geocoder.country+"#"+snapshot_link+"#"+str(geocoder.latitude)+"#"+str(geocoder.longitude)+"#"+"ERROR"+"#"+"ERROR"
+						pass
+		except:
+			pass
 	listfileUS = open("list_US_webcamgalore.txt", "a")
 	listfileUS.write("country#state#city#snapshot_url#latitude#longitude\n")
 	time.sleep(0.5)
 	for i in USLinks:
 		driver.get(i)
-		locationString = driver.get_text_excluding_children(driver, driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/h1"))
-		if "Several" not in locationString:
-			country = locationString.split(":")[0].split(",")[-1].strip()
-			city = locationString.split(":")[0].split(",")[0].strip()
-			locat = locationString.split(":")[1].strip()
-			locatwords = locat.split()
-			resultwords  = [word for word in locatwords if word.lower() not in stopwords]
-			locat = ' '.join(resultwords)
-			state = locationString.split(":")[0].split(",")[1].strip()
-			snapshot_link = urllib.quote(driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/table[2]/tbody/tr/td[@style='padding-left:10px']/center/a/img").get_attribute("src"), safe = ':?,=/&')
-			try:
+		try:
+			locationString = driver.get_text_excluding_children(driver, driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/h1"))
+			if "Several" not in locationString:
+				country = locationString.split(":")[0].split(",")[-1].strip()
+				city = locationString.split(":")[0].split(",")[0].strip()
+				locat = locationString.split(":")[1].strip()
+				locatwords = locat.split()
+				resultwords  = [word for word in locatwords if word.lower() not in stopwords]
+				locat = ' '.join(resultwords)
+				state = locationString.split(":")[0].split(",")[1].strip()
 				snapshot_link = urllib.quote(driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/table[2]/tbody/tr/td[@style='padding-left:10px']/center/a/img").get_attribute("src"), safe = ':?,=/&')
-			except:
-				snapshot_link = urllib.quote(driver.find_element_by_class_name("webcam-image").get_attribute("src"), safe = ':?,=/&')
-			try:
-				geocoder.locateCoords(locat, city, state, country)
-				geocoder.reverse(geocoder.latitude, geocoder.longitude)
-				info = geocoder.country + '#' + geocoder.state + '#' + geocoder.city + '#' + snapshot_link + '#' + str(geocoder.latitude) + '#' + str(geocoder.longitude)
-				listfileUS.write(info.encode('utf-8').replace(" ","").replace("##", "#").replace("\n",'')+'\n')
-				time.sleep(1.0)
-			# API key cycling courtesy of Ryan
-			except Exception as error:
-				if str(error) == "Your request was denied.":
-					API_num = API_num+1
-					if API_num < len(geocodingKeys):
-						geocoder = Geocoding("Google", geocodingKeys[API_num])
+				print locat
+				locat = locat.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				state = state.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				city = city.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				country = country.encode('utf-8').replace("ë","e").replace("ö","o").replace("ß","s").replace("Ö","O").replace("Ë","E").replace("ẞ","S").replace("ş","s").replace("Ş","S").replace("Ç","C").replace("ç","c").replace("ı","i").replace("İ","I").replace("â","a").replace("é","e").replace("Î","I").replace("î","i").replace("É","E").replace("è","e").replace("È","E").replace("À","A").replace("à","a").replace("ü","u").replace("Ü","U").replace("ğ","g").decode('utf-8')
+				try:
+					snapshot_link = urllib.quote(driver.find_element_by_xpath("/html/body/table/tbody/tr/td[@valign='top']/table[2]/tbody/tr/td[@style='padding-left:10px']/center/a/img").get_attribute("src"), safe = ':?,=/&')
+				except:
+					try:
+						snapshot_link = urllib.quote(driver.find_element_by_class_name("webcam-image").get_attribute("src"), safe = ':?,=/&')
+					except:
+						pass
+				try:
+					geocoder.locateCoords(locat, city, state, country)
+					time.sleep(1.5)
+					geocoder.reverse(geocoder.latitude, geocoder.longitude)
+					time.sleep(1.5)
+					info = geocoder.country + '#' + geocoder.state + '#' + geocoder.city + '#' + snapshot_link + '#' + str(geocoder.latitude) + '#' + str(geocoder.longitude)
+					listfileUS.write(info.replace(" ","").replace("##", "#").replace("\n",'')+'\n')
+					time.sleep(1.0)
+				# API key cycling courtesy of Ryan
+				except Exception as error:
+					if str(error) == "Your request was denied." or str(error) == "The given key has gone over the requests limit in the 24 hour period or has submitted too many requests in too short a period of time.":
+						API_num = API_num+1
+						if API_num < len(geocodingKeys):
+							geocoder = Geocoding("Google", geocodingKeys[API_num])
+						else:
+							print "<<<ERROR:"+str(error)+">>>"
+							print "API list exhausted, stopped on:"
+							print geocoder.country+"#"+snapshot_link+"#"+str(geocoder.latitude)+"#"+str(geocoder.longitude)+"#"+"ERROR"+"#"+"ERROR"
+							return
 					else:
-						print "<<<ERROR:"+str(error)+">>>"
-						print "API list exhausted, stopped on:"
-						print country+"#"+url+"#"+str(latitude)+"#"+str(longitude)+"#"+"ERROR"+"#"+"ERROR"
-						return
-				else:
-					print "<<<Error:"+str(error)+">>>"+" For:\n"+country+"#"+url+"#"+str(latitude)+"#"+str(longitude)+"#"+"ERROR"+"#"+"ERROR"
-					pass
+						print "<<<Error:"+str(error)+">>>"+" For:\n"+geocoder.country+"#"+snapshot_link+"#"+str(geocoder.latitude)+"#"+str(geocoder.longitude)+"#"+"ERROR"+"#"+"ERROR"
+						pass
+		except:
+			pass
 	driver.close()
 
 if __name__ == '__main__':
