75766bfc990965f534df09be5f520f09d444c8ac
Ajay1997 <ajayGkumar97@gmail.com>
2017-04-23 16:00:32 -0400

    Fixed spelling

--- wrapper.py
+++ wrapper.py
@@ -171,6 +171,6 @@ def wrapper(filename,option):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f','--filename',help="Name of CSV file containing camera info with .csv extension",type = str)
-    parser.add_argument('-o','--option',help="Enter 1 to use wrapper_info file to grab camera images from URL, 2 to grab video data and 3 to search camera database to grab images",type = int)
+    parser.add_argument('-o','--option',help="Enter 1 to use wrapper_info file to grab camera images from URL, 2 to grab video data or 3 to search camera database to grab images",type = int)
     args = parser.parse_args()
     wrapper(args.filename,args.option)
