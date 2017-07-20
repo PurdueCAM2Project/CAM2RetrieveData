81e7483826d8f10bf5c34face3379cd383c72fc8
rschlueter98 <rschlueter98@gmail.com>
2017-07-20 15:08:08 -0400

    Added threading for downloading and saving images

--- Retrieval/Code/streamDownload.py
+++ Retrieval/Code/streamDownload.py
@@ -16,7 +16,7 @@ def loadStreams():
             if (len(threadList)<numCores):
                 t = threading.Thread(target=addFeed, args=(line,))
                 t.start()
-                print ("Thread Started")
+                # print ("Thread Started")
                 threadList.append(t)
                 opened=True
             else:
@@ -31,7 +31,7 @@ def addFeed(url):
     if (cap.isOpened()):
         streamQueue.append(cap)
     threadList.pop()
-    print ("Thread Released")
+    # print ("Thread Released")
 
 def downloadImages(streamQueue):
     raw_input("Press enter to download images")
@@ -41,29 +41,39 @@ def downloadImages(streamQueue):
     # framesSaved = 0
     breaker = False
     totalNumImages = 500
-    totalNumStreams = len(streamQueue)
-
-    while (True):
-        try:
-            stream = streamQueue.pop()
-            for x in range(totalNumStreams):
-
-                # image = stream.read()[1]
-                # imageData.append(image)
-        except Exception as e:
-            print ("Breaking Exception: " + str(e))
-            breaker=True
-            break
-        if breaker:
-            break
+    print len(streamQueue)
+    for x in range(len(streamQueue)):
+        #
+        opened = False
+        while (not opened):
+            if (len(threadList)<numCores):
+                t = threading.Thread(target=saveFrame, args=(streamQueue[x], imageData, totalNumImages, len(streamQueue),))
+                t.start()
+                # print ("Thread Started")
+                threadList.append(t)
+                opened = True
+            else:
+                time.sleep(0.1)
+    while (len(threadList) != 0):
+        time.sleep(0.1)
+        # while (True):
+        #     try:
+        #         saveFrame(streamQueue[x], imageData, totalNumImages, len(streamQueue))
+        #     except Exception as e:
+        #         print ("Breaking Exception: " + str(e))
+                # breaker=True
+                # break
+            # if breaker:
+            #     break
     print ("Ellapsed time: " + str(time.time()-tick))
     saveImages(imageData)
 
 
-def saveFrame (stream, imageData):
-    for x in range(20):
+def saveFrame (stream, imageData, images, streams):
+    for x in range(images/streams):
         image = stream.read()[1]
         imageData.append(image)
+    # print ("Thread freed")
     threadList.pop()
 
 
@@ -72,22 +82,46 @@ def saveImages(imageData):
     sys.stdout.flush()
     tick = time.time()
     fileNumber = 0
-    for frame in imageData:
-        fileName = ("z_img no" + str(fileNumber) + ".jpg")
-        cv2.imwrite(fileName, frame)
-        fileNumber += 1
+    # for frame in imageData:
+    #
+    #     fileName = ("z_img no" + str(fileNumber) + ".jpg")
+    #     cv2.imwrite(fileName, frame)
+    #     fileNumber += 1
+    #
+
+    for threadNo in range (numCores):
+        opened = False
+        while (not opened):
+            if (len(threadList)<numCores):
+                t = threading.Thread(target=saveImage, args=(imageData, threadNo,))
+                t.start()
+                # print ("Thread Started")
+                threadList.append(t)
+                opened = True
+            else:
+                time.sleep(0.1)
+
+    # Sleep until all threads are released
+    while (len(threadList) != 0):
+        time.sleep(0.1)
+
     print ("Ellapsed time: " + str(time.time() - tick))
+
+
+def saveImage(imageData, threadNo):
+    for fileNumber in range (100):
+        try:
+            frame = imageData.pop()
+            fileName = ("z_" + "threadNumber" + str(threadNo) + "iamgeNumber" + str(fileNumber) + ".jpg")
+            cv2.imwrite(fileName, frame)
+        except:
+            break
+    threadList.pop()
 
 
 if __name__ == '__main__':
     print ("Starting")
     loadStreams()
     print ("Ending")
-
-
-
-
-
-
 
 
