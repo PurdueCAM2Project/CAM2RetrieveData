9102554359b750c67b28e68c6201f6303be157da
Minghao Guo <guo288@purdue.edu>
2017-08-01 20:41:12 -0400

    Update loop.py

--- Retrieval/Streamlink/loop.py
+++ Retrieval/Streamlink/loop.py
@@ -5,15 +5,11 @@ Descriptive Name     : loop.py
 Author               : Minghao Guo (Mina)								      
 Contact Info         : guo288@purdue.edu
 Date Written         : June 22, 2017
-Description          : Used for looping through all the earthcamURLs and transferred them into m3u8 urls by using fetchstreamsrc.py
+Description          : Used for looping through all the streaming webcam urls and transferring them into m3u8 urls by using fetchstreamsrc.py
 Command to run script: python3 loop.py
 Usage                : Can be imported into another script or run from the command line
 Input file format    : N/A
 Output               : A list of m3u8 urls
-Note                 : This is only the initial script for getting all the m3u8 urls, it will stuck everytime it encounters an url of 				RTMP type, so I had to run fetchstreamsrc.py manually to get the information of those urls with issues. The 				next step is to identify the urls of RTMP type and deal with them specifically.
-Other files required by : fetchstreamsrc.py (written by Caleb)
-this script and where 
-located
 --------------------------------------------------------------------------------
 """
 
@@ -43,8 +39,7 @@ with open('earthcamURL.txt') as f:
 			#print(scheme)
 #transform the original urls into m3u8 ones using the function in fetchstreamsrc.py
         try:
-            #src_url = []
-        
+            #src_url = []        
             src_url = get_stream_src_from_url(line.rstrip())
         #print(src_url)
         except Exception as e:
