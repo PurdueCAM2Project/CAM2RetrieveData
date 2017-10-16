02c3e33583e6930ead520e1f3b03646dfcdd365c
Sam Yellin <syellin@purdue.edu>
2017-10-16 11:29:13 -0400

    clean up in preparation for merge to master

--- Retrieval/Code/src/ImageArchiver/test_archiver.py
+++ Retrieval/Code/src/ImageArchiver/test_archiver.py
@@ -1,10 +1,11 @@
 '''
 Example usage of archiver class to retrieve one frame per second for 100 seconds from each camera specified in db_test.csv.
 '''
+import os
 
 from ImageArchiver import ImageArchiver
 
-archiver = ImageArchiver("localhost", "root", "Tiger007", "cam2")
+archiver = ImageArchiver("localhost", "root", os.environ['DB_PASS'], "cam2")
 
-#archiver.retrieve_csv("cameraSources/test_camera_url", 10, 1, "results")
-archiver.retrieve_db("cameraSources/puerto_rico.csv",10 ,1, "/results")
+#archiver.retrieve_csv("cameraSources/test_camera_url", 10, 1, "results/")
+archiver.retrieve_db("cameraSources/db_test.csv", 10, 1, "results/")
